e88a6fba2d47428fbb8a3e81eac9ebf0ad8b785f
Kramzin <kramzin.ru@gmail.com>
2021-05-25 01:58:09 +0300

    Implement signup

--- src/pages/sign-up.js
+++ src/pages/sign-up.js
@@ -41,20 +41,28 @@ export default function SignUp() {
           {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
 
           <form onSubmit={handleSignUp} method="POST">
+          <input
+              aria-label="Enter your full name"
+              type="text"
+              placeholder="Full Name"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              onChange={({ target }) => setFullName(target.value)}
+              value={fullName}
+            />
+          <input
+              aria-label="Enter your username"
+              type="text"
+              placeholder="Username"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              onChange={({ target }) => setUsername(target.value)}
+              value={username}
+            />
             <input
               aria-label="Enter your email address"
               type="text"
               placeholder="Email address"
               className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
               onChange={({ target }) => setEmailAddress(target.value)}
-              value={emailAddress}
-            />
-            <input
-              aria-label="Enter your full name"
-              type="text"
-              placeholder="Full Name"
-              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
-              onChange={({ target }) => setFullName(target.value)}
               value={emailAddress}
             />
             <input
@@ -78,8 +86,8 @@ export default function SignUp() {
         <div className="flex justify-center items-center flex-col w-full bg-white p-4 rounded-full border border-gray-primary">
           <p className="text-sm">
             Have an account?{` `}
-            <Link to={ROUTES.SIGN_UP} className="font-bold text-blue-medium">
-              Sign up
+            <Link to={ROUTES.LOGIN} className="font-bold text-blue-medium">
+              Log in
             </Link>
           </p>
         </div>
