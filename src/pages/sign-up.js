2e0e9e7c89522f1f04616fe1ea12b2644ef14dd8
Kramzin <kramzin.ru@gmail.com>
2021-05-25 18:10:47 +0300

    Implement NotFound & auth conditions

--- src/pages/sign-up.js
+++ src/pages/sign-up.js
@@ -2,6 +2,7 @@ import { useState, useContext, useEffect } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import FirebaseContext from '../context/firebase';
 import * as ROUTES from '../constants/routes';
+import { doesUsernameExist } from '../services/firebase';
 
 export default function SignUp() {
   const history = useHistory();
@@ -18,8 +19,43 @@ export default function SignUp() {
   const handleSignUp = async (event) => {
     event.preventDefault();
 
-    try {
-    } catch (error) {
+    const usernameExists = await doesUsernameExist(username);
+    if (!usernameExists) {
+      try {
+        const createdUserResult = await firebase
+          .auth()
+          .createUserWithEmailAndPassword(emailAddress, password);
+
+        // authentication
+        // -> emailAddress & password & username (displayName)
+        await createdUserResult.user.updateProfile({
+          displayName: username
+        });
+
+        // firebase user collection (create a document)
+        await firebase
+          .firestore()
+          .collection('users')
+          .add({
+            userId: createdUserResult.user.uid,
+            username: username.toLowerCase(),
+            fullName,
+            emailAddress: emailAddress.toLowerCase(),
+            following: ['2'],
+            followers: [],
+            dateCreated: Date.now()
+          });
+
+        history.push(ROUTES.DASHBOARD);
+      } catch (error) {
+        setFullName('');
+        setEmailAddress('');
+        setPassword('');
+        setError(error.message);
+      }
+    } else {
+      setUsername('');
+      setError('That username is already taken, please try another.');
     }
   };
 
@@ -41,21 +77,21 @@ export default function SignUp() {
           {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
 
           <form onSubmit={handleSignUp} method="POST">
-          <input
-              aria-label="Enter your full name"
-              type="text"
-              placeholder="Full Name"
-              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
-              onChange={({ target }) => setFullName(target.value)}
-              value={fullName}
-            />
-          <input
+            <input
               aria-label="Enter your username"
               type="text"
               placeholder="Username"
               className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
               onChange={({ target }) => setUsername(target.value)}
               value={username}
+            />
+            <input
+              aria-label="Enter your full name"
+              type="text"
+              placeholder="Full name"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              onChange={({ target }) => setFullName(target.value)}
+              value={fullName}
             />
             <input
               aria-label="Enter your email address"
@@ -87,7 +123,7 @@ export default function SignUp() {
           <p className="text-sm">
             Have an account?{` `}
             <Link to={ROUTES.LOGIN} className="font-bold text-blue-medium">
-              Log in
+              Login
             </Link>
           </p>
         </div>
